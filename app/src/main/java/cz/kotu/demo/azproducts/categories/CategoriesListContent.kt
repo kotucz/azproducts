d4b02b2087abb365219d675d82d7f89d9e185213
Tomas Kotula <tomasrkotula@gmail.com>
2021-10-16 22:13:32 +0200

    New lists design

--- app/src/main/java/cz/kotu/demo/azproducts/categories/CategoriesListContent.kt
+++ app/src/main/java/cz/kotu/demo/azproducts/categories/CategoriesListContent.kt
@@ -1,19 +1,26 @@
 package cz.kotu.demo.azproducts.categories
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cz.kotu.demo.azproducts.loading.LoadingState
 import cz.kotu.demo.azproducts.loading.LoadingStateContainer
+import cz.kotu.demo.azproducts.ui.theme.AZProductsTheme
 
 @Composable
 fun CategoriesListContent(
@@ -25,16 +32,42 @@ fun CategoriesListContent(
     )
 
     LoadingStateContainer(categoriesState) { categories: List<Category> ->
-        LazyColumn(
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
-        ) {
-            items(
-                items = categories,
-                itemContent = { category ->
-                    CategoryListItem(category, onClick = { onCategoryClick(category) })
-                }
-            )
-        }
+        CategoriesList(categories, onCategoryClick)
+    }
+}
+
+@Composable
+private fun CategoriesList(
+    categories: List<Category>,
+    onCategoryClick: (Category) -> Unit
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        items(
+            items = categories,
+            itemContent = { category ->
+                CategoryListItem(category, onClick = { onCategoryClick(category) })
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoriesListPreview() {
+    AZProductsTheme {
+        CategoriesList(listOf(
+            Category(1, "Alfa"),
+            Category(2, "Bravo"),
+            Category(3, "Charlie"),
+            Category(4, "Delta"),
+            Category(5, "Foxtrot"),
+            Category(6, "Uniform"),
+            Category(7, "Charlie"),
+            Category(8, "Kilo"),
+        ), onCategoryClick = {})
     }
 }
 
@@ -42,10 +75,24 @@ fun CategoriesListContent(
 fun CategoryListItem(
     category: Category, onClick: () -> Unit
 ) {
-    Column {
+    Surface(
+        Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+        shape = MaterialTheme.shapes.medium,
+        color = Color.LightGray,
+    ) {
         Text(
-            text = category.title, style = typography.h6,
-            modifier = Modifier.clickable(onClick = onClick)
+            text = category.title, style = typography.h4,
+            modifier = Modifier.padding(8.dp)
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryListItemPreview() {
+    AZProductsTheme {
+        CategoryListItem(Category(1, "Ponožky"), onClick = {})
     }
 }
--- app/src/main/java/cz/kotu/demo/azproducts/products/ProductsListContent.kt
+++ app/src/main/java/cz/kotu/demo/azproducts/products/ProductsListContent.kt
@@ -1,16 +1,21 @@
 package cz.kotu.demo.azproducts.products
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cz.kotu.demo.azproducts.loading.LoadingState
 import cz.kotu.demo.azproducts.loading.LoadingStateContainer
@@ -23,7 +28,8 @@ fun ProductsListContent(viewModel: ProductsViewModel, onProductClick: (Product) 
 
     LoadingStateContainer(productsState) { products: List<Product> ->
         LazyColumn(
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
             items(
                 items = products,
@@ -39,10 +45,16 @@ fun ProductsListContent(viewModel: ProductsViewModel, onProductClick: (Product) 
 fun ProductListItem(
     product: Product, onClick: () -> Unit
 ) {
-    Column {
+    Surface(
+        Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+        shape = MaterialTheme.shapes.medium,
+        color = Color.LightGray,
+    ) {
         Text(
-            text = product.name, style = typography.h6,
-            modifier = Modifier.clickable(onClick = onClick)
+            text = product.name, style = typography.h5,
+            modifier = Modifier.padding(8.dp)
         )
     }
 }
